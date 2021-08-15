0639fc7f6913e4563a9e215e46213e857935c62a
vera-weili <tswang@udel.edu>
2021-08-15 14:40:31 -0400

    wip

---
+++ ops_ase_integration_tests/topology.py
@@ -0,0 +1,21 @@
+from openpathsampling.engines import Topology
+
+
+class AseTopology(Topology):
+    """
+    Parameters
+    ----------
+    n_spatial : int
+        the number of spatial degrees of freedom
+    masses : numpy.ndarray (n_atoms, dtype=float)
+        The masses associated with each atom
+    pes : :class:`.PES`
+        potential energy surface for this system
+    n_atoms : int
+        number of atoms (default is 1)
+    """
+
+    def __init__(self, n_spatial, masses, n_atoms=1):
+        super(AseTopology, self).__init__(n_atoms=n_atoms, n_spatial=n_spatial)
+        self.masses = masses
+        # self.pes = pes
---
+++ ops_ase_unit_tests/snapshot_test.py
@@ -0,0 +1,19 @@
+from unittest import TestCase, mock
+
+from ops_ase.snapshot import AseSnapshot
+
+
+class AseSanpshotTest(TestCase):
+    def setUp(self) -> None:
+        self.mock_integrator = mock.Mock()
+        self.mock_integrator.atoms.get_dihedrals.return_value = 1
+
+    def test_initialization(self):
+        snapshot = AseSnapshot(integrator=self.mock_integrator)
+        self.assertIsNone(snapshot.velocities)
+        self.assertIsNone(snapshot.coordinates)
+        self.assertIsNone(snapshot.engine)
+
+    def test_get_dihedrals(self):
+        snapshot = AseSnapshot(integrator=self.mock_integrator)
+        snapshot.get_dihedrals(indices=[[0, 1, 2, 3]])
